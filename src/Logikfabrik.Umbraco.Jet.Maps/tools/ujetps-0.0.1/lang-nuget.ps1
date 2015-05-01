8e22fcb2ff7e38bc16f813957822bef3a7f46ca9
logikfabrik <anton@logikfabrik.se>
2015-05-01 11:52:45 +0200

    Started restructuring NuGet PS.

---
+++ src/Logikfabrik.Umbraco.Jet.Maps/tools/ujetps-0.0.1/lang-nuget.ps1
@@ -0,0 +1,47 @@
+﻿Function Add-Lang
+{
+	<#
+	.SYNOPSIS
+	.DESCRIPTION
+	.PARAMETER umbLangPath
+	.PARAMETER nugLangPath
+	#>
+
+	Param($umbLangPath, $nugLangPath)
+
+	$nugLangFiles = Get-ChildItem "$nugLangPath\*.xml" -Name
+	
+	foreach ($nugLangFile in $nugLangFiles) {
+		if (-Not (Test-Path "$umbLangPath\$nugLangFile")) {
+			continue
+		}
+		
+		$umbLangXml = New-Object XML
+		$umbLangXml.Load("$umbLangPath\$nugLangFile")
+		
+		$nugLangXml = New-Object XML
+		$nugLangXml.Load("$nugLangPath\$nugLangFile")
+		
+		foreach ($area in Get-LangAreas $nugLangXml) {
+			$keys = Get-LangAreaKeys $nugLangXml $area
+			
+			foreach ($key in $keys.GetEnumerator()) {
+				Add-LangAreaKey $umbLangXml $area $key.Key $key.Value
+			}
+		}
+		
+		$umbLangXml.Save("$umbLangPath\$nugLangFile")
+	}
+}
+
+Function Remove-Lang
+{
+	<#
+	.SYNOPSIS
+	.DESCRIPTION
+	.PARAMETER umbLangPath
+	.PARAMETER nugLangPath
+	#>
+
+	Param($umbLangPath, $nugLangPath)
+}
